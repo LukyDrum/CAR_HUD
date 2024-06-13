01492b9e792690ba31c8d91205497f25a73f6954
Luky Drum <lukbubi12@gmail.com>
2024-06-13 22:17:38 +0200

    Add button to start android auto

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import customtkinter as ctk
+import subprocess
+
+
+def start_android_auto():
+    subprocess.run(["sudo", "autoapp"])
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("./themes/dark-blue.json")
@@ -11,11 +16,17 @@ app.title("Car HUD")
 tabview = ctk.CTkTabview(master=app)
 tabview.pack(padx=20, pady=20, fill="both", expand=True)
 
-tabview.add("Dashboard")
-tabview.add("LEDs")
-tabview.add("Android Auto")
+dash_tab = tabview.add("Dashboard")
+leds_tab = tabview.add("LEDs")
+AA_tab = tabview.add("Android Auto")
 
 tabview.set("Dashboard")
+
+
+# Android Auto tab
+start_butt = ctk.CTkButton(AA_tab, text="Start", command=start_android_auto)
+start_butt.place(relx=0.5, rely=0.5, anchor="center")
+
 
 
 if __name__ == "__main__":
