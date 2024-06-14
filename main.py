2268e93cce830f52e95ee82de4176c680a4c51a8
Luky Drum <lukbubi12@gmail.com>
2024-06-14 12:06:06 +0200

    Add basic dashboard mockup

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def prev_tab():
 FONT_NAME = "Roboto"
 FONT_SIZE_NORMAL = 30
 FONT_SIZE_LARGE = 50
+FONT_SIZE_XXLARGE = 120
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("./themes/dark-blue.json")
 
@@ -39,6 +40,10 @@ ctk.set_default_color_theme("./themes/dark-blue.json")
 app = ctk.CTk()
 app.attributes('-fullscreen', True) # Set fullscreen
 app.title("Car HUD")
+
+# Get the size of the screen
+screen_width = app.winfo_screenwidth()
+screen_height = app.winfo_screenheight()
 
 # Content of the app
 tabview = ctk.CTkTabview(master=app, command=tab_button_clicked)
@@ -58,6 +63,12 @@ AA_tab = tabview.tab("Android Auto")
 start_butt = ctk.CTkButton(AA_tab, text="Start", font=(FONT_NAME, FONT_SIZE_LARGE), command=start_android_auto)
 start_butt.place(relx=0.5, rely=0.5, anchor="center")
 
+# Dashboard tab
+dashboard_tab = tabview.tab("Dashboard")
+speed_label = ctk.CTkLabel(master=dashboard_tab, text="42", font=(FONT_NAME, FONT_SIZE_XXLARGE))
+rpm_progress = ctk.CTkProgressBar(master=dashboard_tab, orientation="horizontal", width=screen_width//2, height=50)
+speed_label.place(relx=0.5, rely=0.4, anchor="center")
+rpm_progress.place(relx=0.5, rely=0.7, anchor="center")
 
 
 if __name__ == "__main__":
