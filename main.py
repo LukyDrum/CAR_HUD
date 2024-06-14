1d0a765b25418c0bc016cbe888fc0596d33f8f9e
Luky Drum <lukbubi12@gmail.com>
2024-06-14 10:05:40 +0200

    Make tabs into a list

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import customtkinter as ctk
 import subprocess
 
 
+# Tabs of the app and the index of the current tab
+tabs = ["Dashboard", "LEDs", "Android Auto"]
+tab_index = 0
+
 def start_android_auto():
     subprocess.run(["sudo", "autoapp"])
 
+
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("./themes/dark-blue.json")
+
+FONT_NAME = "Roboto"
 
 app = ctk.CTk()
 app.attributes('-fullscreen', True) # Set fullscreen
@@ -16,15 +23,12 @@ app.title("Car HUD")
 tabview = ctk.CTkTabview(master=app)
 tabview.pack(padx=20, pady=20, fill="both", expand=True)
 
-dash_tab = tabview.add("Dashboard")
-leds_tab = tabview.add("LEDs")
-AA_tab = tabview.add("Android Auto")
-
-tabview.set("Dashboard")
-
+for tab in tabs:
+    tabview.add(tab)
 
 # Android Auto tab
-start_butt = ctk.CTkButton(AA_tab, text="Start", command=start_android_auto)
+AA_tab = tabview.tab("Android Auto")
+start_butt = ctk.CTkButton(AA_tab, text="Start", font=(FONT_NAME, 50), command=start_android_auto)
 start_butt.place(relx=0.5, rely=0.5, anchor="center")
 
 
