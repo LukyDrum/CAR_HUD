264658796934459a9cfd5b2f4c94e563c58dfc32
Luky Drum <lukbubi12@gmail.com>
2024-06-27 13:07:16 +0200

    Add sliders to control the color of LEDs

--- leds_tab.py
+++ leds_tab.py
@@ -21,8 +21,58 @@ class LEDsTab(Tab):
             offvalue=False,
             font=(FONT_NAME, FONT_SIZE_LARGE),
         )
-        switch.place(relx=0.5, rely=0.5, anchor="center")
+        switch.place(relx=0.5, rely=0.2, anchor="center")
+
+        slider = self.make_color_slider(self.red_slider_event, 0.4, Color("red"))
+        slider.set(self.led_color.get_red() * 255)
+
+        slider = self.make_color_slider(self.green_slider_event, 0.5, Color("green"))
+        slider.set(self.led_color.get_green() * 255)
+
+        slider = self.make_color_slider(self.blue_slider_event, 0.6, Color("blue"))
+        slider.set(self.led_color.get_blue() * 255)
+
+        # Create a block to show the color of the LED
+        color_preview = ctk.CTkLabel(
+            master=self.tab,
+            text=f"HEX {self.led_color.get_hex_l()}",
+            font=(FONT_NAME, FONT_SIZE_LARGE),
+        )
+        color_preview.place(relx=0.5, rely=0.7, anchor="center")
+        self.color_preview = color_preview
 
     def toggle_led(self):
         pass
-    
+
+    def red_slider_event(self, value: int):
+        self.led_color.set_red(value / 255)
+        self.update_color_preview()
+
+    def green_slider_event(self, value: int):
+        self.led_color.set_green(value / 255)
+        self.update_color_preview()
+
+    def blue_slider_event(self, value: int):
+        self.led_color.set_blue(value / 255)
+        self.update_color_preview()
+
+    def make_color_slider(self, method, rely: float, color: Color) -> ctk.CTkSlider:
+        slider = ctk.CTkSlider(
+            master=self.tab,
+            from_=0,
+            to=255,
+            width=self.app.winfo_screenwidth() * 0.4,
+            height=50,
+            command=method,
+            progress_color=color.get_hex(),
+
+        )
+        slider.place(relx=0.5, rely=rely, anchor="center")
+
+        return slider
+
+    def update_color_preview(self) -> None:
+        self.color_preview.configure(
+            text=f"HEX {self.led_color.get_hex_l()}",
+            text_color=self.led_color.get_hex(),
+        )
