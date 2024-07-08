4e864b39153afcfc6ea56a25a4de6479d07cb87c
Luky Drum <lukbubi12@gmail.com>
2024-07-08 22:05:26 +0200

    Implement some methods for bt player

--- bluetooth/bluetooth_player.py
+++ bluetooth/bluetooth_player.py
@@ -1,14 +1,19 @@
+import subprocess
+
 from bluetooth.song_info import SongInfo
+from bluetooth.device import BluetoothDevice
 
 class BluetoothPlayer:
     def __init__(self):
-        pass
+        self.dbus_object = "org.bluez.MediaControl1"
 
     def play(self) -> None:
-        pass
+        base = self._get_base_command()
+        cmd = base + " " + f"{self.dbus_object}.Play"
 
     def pause(self) -> None:
-        pass
+        base = self._get_base_command()
+        cmd = base + " " + f"{self.dbus_object}.Pause"
 
     def play_pause(self) -> None:
         pass
@@ -21,3 +26,14 @@ class BluetoothPlayer:
 
     def song_info(self) -> SongInfo:
         pass
+
+    def _get_active_device(self) -> BluetoothDevice:
+        return BluetoothDevice("A0:46:5A:20:D1:D8", "Motorola Edge 30")
+    
+    def _get_base_command(self) -> str:
+        device = self._get_active_device()
+        return f"dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_{device.address.replace(":", "_")}/player0"
+    
+
+def run_with_timeout(command: str, timeout: float):
+    subprocess.run(command.split(" "), timeout=timeout)
